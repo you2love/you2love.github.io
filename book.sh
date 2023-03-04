43d9f9fe5a95a379341aa5bfbd726f1d25b19631
资深码农 <5300366+deep2code@users.noreply.github.com>
2023-03-04 10:32:32 +0800

    update

---
+++ book.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+# mac
+if [ "$(uname)" == "Darwin" ]; then
+    host=""mac""
+# GNU/Linux操作系统
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    git pull
+    host="linux"
+# Windows NT操作系统
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    # Windows NT操作系统
+    host="windows"
+fi
+
+if [ ${host} == "linux" ]; then
+    cp -r book/* /usr/share/nginx/book/
+fi
